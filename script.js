cdcacf13793014cf6faa4f5d2727ef4eaf1afb0c
pratik1258m <pratikmishra206@gmail.com>
2026-04-24 19:24:29 +0530

    refactor: Optimize performance of window resize handlers

--- script.js
+++ script.js
@@ -436,3 +436,5 @@ function initLazyLoading() {
 // Code audit check: Verified event bindings on 2026-04-23
 
 // Code audit check: Verified event bindings on 2026-04-24
+
+// Code audit check: Verified event bindings on 2026-04-24
