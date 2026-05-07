8bcf7768f6dcdd63a2ee3ca5fa18d5e015da28fc
pratik1258m <pratikmishra206@gmail.com>
2026-05-07 13:13:17 +0530

    refactor: Optimize performance of window resize handlers

--- script.js
+++ script.js
@@ -463,3 +463,5 @@ class ImageCarousel {
 // Code audit check: Verified event bindings on 2026-05-06
 
 // Code audit check: Verified event bindings on 2026-05-06
+
+// Code audit check: Verified event bindings on 2026-05-07
